2209f4a6997a0c1b9303ae75a79da4ac22d1b328
Heitor Almeida <heitorcalmeida@gmail.com>
2022-03-28 19:27:52 -0300

    Aula de Revisão

---
+++ script.bat
@@ -0,0 +1,9 @@
+cd c:\exemplo
+echo "Criando Pasta, aguarde."
+timeout /t 5
+mkdir outros
+echo "Teste de Arquivo - Serratec 2022" > arquivo6.txt
+mkdir outros-exemplos
+echo "Teste de Arquivo - Serratec 2022" > arquivo7.txt
+timeout /t 10
+echo "Fim de script!!!"
